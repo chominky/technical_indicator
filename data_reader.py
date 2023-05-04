ff46d520c09f02b0be879be30a3c24e4902b16d4
mkcho1 <mkcho@airi.kr>
2023-05-04 17:02:17 +0900

    get index ohlcv

--- data_reader.py
+++ data_reader.py
@@ -6,6 +6,9 @@ from tqdm import tqdm
 from pykrx import stock
 from typing import Optional
 
+INDEX_ENG_KOR_MAPPING = {"KOSPI": "코스피",}
+
+
 class MarketData:
     def __init__(self, start_date: str, end_date: str, market: str = "KOSPI"):
         self.start_date = start_date
@@ -16,6 +19,13 @@ class MarketData:
         if date is None:
             date = self.end_date
         return stock.get_market_ticker_list(date, market=self.market)
+    
+    def get_index_ticker_mapping(self):
+        res = {}
+        for index in stock.get_index_ticker_list():
+            res[stock.get_index_ticker_name(index)] = index
+
+        return res
     
     def get_business_days(self):
         code = "XKRX"
@@ -31,6 +41,12 @@ class MarketData:
                 df["Date"] = business_day
                 res = pd.concat([res, df])
                 time.sleep(1)
+        elif ticker in INDEX_ENG_KOR_MAPPING.keys():
+            index_ticker_mapping = self.get_index_ticker_mapping()
+            index_ticker = index_ticker_mapping.get(INDEX_ENG_KOR_MAPPING[ticker])
+            res = stock.get_index_ohlcv(self.start_date, self.end_date, index_ticker)
+            res.reset_index(inplace=True)
+            res.columns.name = None
         else:
             res = stock.get_market_ohlcv(self.start_date, self.end_date, ticker)
             res.reset_index(inplace=True)
@@ -38,7 +54,9 @@ class MarketData:
                                   "시가": "Open", "고가": "High",
                                   "저가": "Low", "종가": "Close",
                                   "거래량": "Volume",
-                                  "거래대금": "Trading value", "등락률": "Change"})
+                                  "거래대금": "Trading value", "등락률": "Change",
+                                  "상장시가총액": "Market Cap"})
+        res.reset_index(drop=True, inplace=True)
         return res
     
     def load_fundamental_data(self, ticker: str):
@@ -56,5 +74,6 @@ class MarketData:
             res["ticker"] = ticker
             res.reset_index(inplace=True)
             res = res.rename(columns={"날짜": "Date"})
+        res.reset_index(drop=True, inplace=True)
         return res
     
