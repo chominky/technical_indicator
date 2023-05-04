a1af4382cb199fdf67c8bbf030a3fa0d2e438c12
mkcho1 <mkcho@airi.kr>
2023-05-04 15:01:31 +0900

    use only pykrx

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,4 @@
-import pandas_datareader.data as pdr
 import exchange_calendars
-import yfinance as yf
 import pandas as pd
 import time
 
@@ -9,13 +7,15 @@ from pykrx import stock
 from typing import Optional
 
 class MarketData:
-    def __init__(self, start_date, end_date, market="KOSPI"):
+    def __init__(self, start_date: str, end_date: str, market: str = "KOSPI"):
         self.start_date = start_date
         self.end_date = end_date
         self.market = market
 
-    def get_ticker_list(self):
-        return stock.get_market_ticker_list(self.end_date, market=self.market)
+    def get_ticker_list(self, date: Optional[str] = None):
+        if date is None:
+            date = self.end_date
+        return stock.get_market_ticker_list(date, market=self.market)
     
     def get_business_days(self):
         code = "XKRX"
@@ -23,15 +23,22 @@ class MarketData:
     
     def load_data(self, ticker: str):
         if ticker == "all":
-            ticker_list = self.get_ticker_list()
+            business_day_list = self.get_business_days()
             res = pd.DataFrame()
-            for ticker in tqdm(ticker_list):
-                df = pdr.DataReader(ticker, 'naver', start=self.start_date, end=self.end_date)
-                df["ticker"] = ticker
+            for business_day in tqdm(business_day_list):
+                df = stock.get_market_ohlcv(business_day, market=self.market)
+                df.reset_index(inplace=True)
+                df["Date"] = business_day
                 res = pd.concat([res, df])
+                time.sleep(1)
         else:
-            res = pdr.DataReader(ticker, 'naver', start=self.start_date, end=self.end_date)
-            res["ticker"] = ticker
+            res = stock.get_market_ohlcv(self.start_date, self.end_date, ticker)
+            res.reset_index(inplace=True)
+        res = res.rename(columns={"티커": "Ticker", "날짜": "Date",
+                                  "시가": "Open", "고가": "High",
+                                  "저가": "Low", "종가": "Close",
+                                  "거래량": "Volume",
+                                  "거래대금": "Trading value", "등락률": "Change"})
         return res
     
     def load_fundamental_data(self, ticker: str):
@@ -44,11 +51,10 @@ class MarketData:
                 res = pd.concat([res, df])
                 time.sleep(1)
             res = res.rename(columns={"티커": "ticker"})
-            res = res.set_index("Date")
-            res.index = pd.DatetimeIndex(res.index)
         else:
             res = stock.get_market_fundamental(self.start_date, self.end_date, ticker)
             res["ticker"] = ticker
-            res.index.name = "Date"
+            res.reset_index(inplace=True)
+            res = res.rename(columns={"날짜": "Date"})
         return res
     
